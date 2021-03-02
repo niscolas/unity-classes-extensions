279d7db944dd7c7c6e11fcc9ecbf5f1dacf8b616
Nícolas Catarina Parreiras <nic.cp@hotmail.com>
2021-03-02 11:18:55 -0300

    adds collider extensions

---
+++ ColliderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Plugins {
+	public static class ColliderExtensions {
+		public static Vector3 GetRandomPoint(this Collider collider) {
+			Bounds bounds = collider.bounds;
+			
+			Vector3 point = new Vector3(
+				Random.Range(bounds.min.x, bounds.max.x),
+				Random.Range(bounds.min.y, bounds.max.y),
+				Random.Range(bounds.min.z, bounds.max.z)
+			);
+
+			if (point != collider.ClosestPoint(point)) {
+				point = GetRandomPoint(collider);
+			}
+
+			return point;
+		}
+	}
+}
